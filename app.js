b17a967e21d0524a9e43cd3492a985edc58b16d4
martind <martind@uninorte.edu.co>
2025-04-08 19:36:55 -0500

    Primera funcion usando express

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { addBook } = require("./index");
 const app = express();
 const port = 3000 ;
 
+//middleware
+app.use(bodyParser.json());
+
+
+//rutas
+app.get('/',( req, res ) => {
+    res.send("Hola mundo")
+})
+
+app.post('/agregar', addBook);
+
+
+// Server
 app.listen( port, ()=>{
     console.log("Servidor corriendo en el puerto", port);
-})
+});
+
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
+const { response, request } = require("express");
 
-var serviceAccount = require("./herramientasoft-bae8e-firebase-adminsdk-fbsvc-c4f3376084.json");
+var serviceAccount = require("./herramientasoft-bae8e-firebase-adminsdk-fbsvc-4dc1df3257.json");
 const { error } = require("console");
 
 admin.initializeApp({
@@ -10,3 +11,27 @@ admin.initializeApp({
 const database = admin.firestore();
 
 const bibliotecaREF = database.collection("biblioteca");
+
+const addBook = async ( req = request, resp = response) => {
+   
+  try {
+    const book = req.body; 
+    const docRef = await bibliotecaREF.add(book);
+    resp.status(200).json({
+      "msg":"Se ha creado un libro exitosamente.",
+      "id": docRef.id
+    })
+  } catch (error) {
+     resp.status(500).json({
+      "msg":"Error al añadir un libro",
+      error
+     })
+  }
+
+}
+
+module.exports ={
+  addBook
+}
+
+
